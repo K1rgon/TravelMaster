983f07d2987f2b83467f0bfb41b563f3f7a18658
Kirgon <zinzo131@gmail.com>
2022-11-04 23:09:56 +0300

    something change

--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import store from './store/store';
 import App from './App';
+import mapsSizeContext from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      {/* <mapsSizeContext.Provider> */}
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
+      {/* </mapsSizeContext.Provider> */}
     </BrowserRouter>
   </Provider>,
 );
