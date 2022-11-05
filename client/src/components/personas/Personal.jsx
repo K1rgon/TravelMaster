51e173509f89e2b82114302f143651e069cba79e
Daniil Pylenok <danila.pwork@mail.ru>
2022-11-05 13:57:50 +0300

    personal-page reales-2

--- client/src/components/personas/Personal.jsx
+++ client/src/components/personas/Personal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './Personal.module.css';
 import image from '../../image/user.png';
 
@@ -13,8 +14,8 @@ export default function Personal() {
     foto: image,
   };
 
-  const routes = [{ start: '123', end: '321' }, { start: '1234', end: '4321' }];
-  const cars = [{ mark: 'audi', year: '2008' }];
+  const routes = [{ start: '123', end: '321', id: 34 }, { start: '1234', end: '4321', id: 24 }];
+  const cars = [{ mark: 'Audi', year: '2008' }];
 
   return (
     <div className={style.block}>
@@ -22,19 +23,23 @@ export default function Personal() {
         <img src={user.foto} width="200px" height="200px" alt="Фото не удалось загрузить" />
         <div className={style.userphoto} />
         <div className={style.username}>{`${user.name} ${user.surname}`}</div>
-        <div className={style.username}>{`${user.email}`}</div>
+        <div className={style.username}>
+          {' '}
+          Ваша почта:
+          {`${user.email}`}
+        </div>
       </div>
       <div className={style.blockright}>
         <div className={style.routes}>
           Ваши маршруты
           {routes.map((rout, index) => (
-            <div id={index}>{`Начало маршрута :${rout.start} , конец маршрута : ${rout.end}`}</div>
+            <Link className={style.rout} to={`/routes/${rout.id}`} id={index}>{`Начало маршрута : ${rout.start} , конец маршрута : ${rout.end}`}</Link>
           ))}
         </div>
         <div className={style.cars}>
           Ваш гараж
           {cars.map((car, index) => (
-            <div id={index}>{`Марка :${car.mark} , год выпуска : ${car.year}`}</div>
+            <div className={style.car} id={index}>{`Марка : ${car.mark} , год выпуска : ${car.year}`}</div>
           ))}
         </div>
       </div>
